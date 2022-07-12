98c2600f3115a055cf9bc072d992591a4f70df25
lionbach <leomumbach@gmail.com>
2022-07-12 15:07:01 -0300

    Primer commit

---
+++ src/main/java/com/alkemy/disney/characters/DisneyCharactersApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.disney.characters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyCharactersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyCharactersApplication.class, args);
+	}
+
+}
